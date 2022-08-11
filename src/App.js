46bc6afdef02689b75cbc5d4cb18af948a43b232
SoJaeBeom <sojb0826@gmail.com>
2022-08-11 14:58:12 +0900

    Add 페이지 Merge

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "./pages/Detail";
 import Main from "./pages/Main";
 import Edit from "./pages/Edit";
-import React from 'react';
+import Add from "./pages/Add";
+import React from "react";
 
 const App = () => {
   return (
@@ -12,10 +13,10 @@ const App = () => {
         <Route path="/" exact element={<Main />} />
         <Route path="detail/:id" element={<Detail />} />
         <Route path="edit/:id" element={<Edit />} />
+        <Route path="post" element={<Add />} />
       </Routes>
     </BrowserRouter>
   );
 };
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-<<<<<<< HEAD
 import store from "./redux/config/configStore";
-=======
-import store from './redux/config/configStore';
->>>>>>> 3cc3809bdb45f46100420b0cf369a99def50c54f
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 3cc3809bdb45f46100420b0cf369a99def50c54f
